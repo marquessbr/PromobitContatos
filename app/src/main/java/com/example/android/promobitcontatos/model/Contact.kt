d363129dea60511760df3e0c06bc175c5750175c
armando Marques Sobrinho <marquessbr@gmail.com>
2020-05-28 10:31:32 -0300

    Craiada a classe contacto

---
+++ app/src/main/java/com/example/android/promobitcontatos/model/Contact.kt
@@ -0,0 +1,36 @@
+package com.example.android.promobitcontatos.model
+
+class Contact(
+    id: Int,
+    name: String,
+    company: String,
+    photo : String,
+    new: Boolean
+) {
+
+    private var mId: Int = id
+    private var mName: String = name
+    private var mCompany: String = company
+    private var mPhoto : String = photo
+    private var mNew: Boolean = new
+
+    fun getId(): Int {
+        return this.mId
+    }
+
+    fun getName(): String {
+        return this.mName
+    }
+
+    fun getCompany(): String {
+        return this.mCompany
+    }
+
+    fun getPhoto(): String {
+        return this.mPhoto
+    }
+
+    fun getNew(): Boolean {
+        return this.mNew
+    }
+}
